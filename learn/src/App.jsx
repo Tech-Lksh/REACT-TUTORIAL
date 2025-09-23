14dd8701f06a52552041466b14420702934e822c
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-09-23 04:28:11 +0000

    React

--- learn/src/App.jsx
+++ learn/src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <h1>
-      Hello react 
-    </h1>
-  )
-}
+    <div>
+      <h2>Count is : {count}</h2>
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click
+      </button>
+    </div>
+  );
+};
 
-export default App
+export default App;
